a23497b05172db65a9cfdaf0b4477f43da4d5114
Mohammed Adnan <adnanmcj@gmail.com>
2017-08-13 14:20:50 -0400

    Added existing codebase

---
+++ assets/js/scripts.js
@@ -0,0 +1,197 @@
+/*
+* ----------------------------------------------------------------------------------------
+Author       : DuezaThemes
+Author URL   : https://themeforest.net/user/duezathemes
+Template Name: Medical Care - Hospital, clinic, Health & Medical Template
+Version      : 1.0                                          
+* ----------------------------------------------------------------------------------------
+*/
+
+(function ($) {
+    'use strict';
+
+    jQuery(document).ready(function () {
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  PRELOADER JS
+         * ----------------------------------------------------------------------------------------
+         */
+        $(window).on('load', function () {
+            $('.preloader').fadeOut();
+            $('.preloader-area').delay(350).fadeOut('slow');
+        });
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  CHANGE MENU BACKGROUND JS
+         * ----------------------------------------------------------------------------------------
+         */
+        $(window).on('scroll', function () {
+            if ($(window).scrollTop() > 200) {
+                $('.header-top-area').addClass('menu-bg');
+            } else {
+                $('.header-top-area').removeClass('menu-bg');
+            }
+        });
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  PROGRESS BAR JS
+         * ----------------------------------------------------------------------------------------
+         */
+        $('.progress-bar > span').each(function () {
+            var $this = $(this);
+            var width = $(this).data('percent');
+            $this.css({
+                'transition': 'width 3s'
+            });
+            setTimeout(function () {
+                $this.appear(function () {
+                    $this.css('width', width + '%');
+                });
+            }, 500);
+        });
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  SMOTH SCROOL JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $('a.smoth-scroll').on("click", function (e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+            e.preventDefault();
+        });
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  WORK JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $('.work-inner').mixItUp();
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  MAGNIFIC POPUP JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $('.work-popup').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+
+        });
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  PARALLAX JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $(window).stellar({
+            responsive: true,
+            positionProperty: 'position',
+            horizontalScrolling: false
+        });
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  COUNTER UP JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $('.counter-num').counterUp();
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  TESTIMONIAL JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $(".testimonial-list").owlCarousel({
+            items: 2,
+            autoPlay: true,
+            navigation: false,
+            itemsDesktop: [1199, 1],
+            itemsDesktopSmall: [980, 1],
+            itemsTablet: [768, 1],
+            itemsTabletSmall: false,
+            itemsMobile: [479, 1],
+            pagination: true,
+            autoHeight: true,
+        });
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  EXTRA JS
+         * ----------------------------------------------------------------------------------------
+         */
+        $(document).on('click', '.navbar-collapse.in', function (e) {
+            if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+                $(this).collapse('hide');
+            }
+        });
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        });
+
+        // make col same heights
+        $( document ).ready(function() {
+            var heights = $(".single-feature").map(function() {
+                return $(this).height();
+            }).get(),
+
+            maxHeight = Math.max.apply(null, heights);
+
+            $(".single-feature").height(maxHeight);
+        });
+        
+        $( document ).ready(function() {
+            var heights = $(".single-contact-details").map(function() {
+                return $(this).height();
+            }).get(),
+
+            maxHeight = Math.max.apply(null, heights);
+
+            $(".single-contact-details").height(maxHeight);
+        });
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  SCROOL TO UP JS
+         * ----------------------------------------------------------------------------------------
+         */
+        $(window).on('scroll', function () {
+            if ($(this).scrollTop() > 250) {
+                $('.scrollup').fadeIn();
+            } else {
+                $('.scrollup').fadeOut();
+            }
+        });
+        $('.scrollup').on("click", function () {
+            $("html, body").animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  WOW JS
+         * ----------------------------------------------------------------------------------------
+         */
+        new WOW().init();
+    });
+
+})(jQuery);
